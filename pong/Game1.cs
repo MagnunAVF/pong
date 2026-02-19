01bc91b3622685a3dd1e67a5db658c4be13e41fa
MagnunAVF <magnunavf@gmail.com>
2026-02-19 00:58:06 -0300

    Implement game loop logic

--- pong/Game1.cs
+++ pong/Game1.cs
@@ -8,17 +8,68 @@ public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private Texture2D _pixelTexture;
+    private SpriteFont _font;
+
+    private Paddle _player1;
+    private Paddle _player2;
+    private Ball _ball;
+    private ScoreManager _scoreManager;
+
+    private const int ScreenWidth = 800;
+    private const int ScreenHeight = 600;
+    private const int PaddleWidth = 15;
+    private const int PaddleHeight = 100;
+    private const int BallSize = 15;
+    private const int PaddleOffset = 30;
+
+    private enum GameState { Playing, GameOver }
+    private GameState _gameState;
+    private KeyboardState _previousKeyboardState;
 
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+
+        _graphics.PreferredBackBufferWidth = ScreenWidth;
+        _graphics.PreferredBackBufferHeight = ScreenHeight;
     }
 
     protected override void Initialize()
     {
-        // TODO: Add your initialization logic here
+        _player1 = new Paddle(
+            PaddleOffset,
+            (ScreenHeight - PaddleHeight) / 2,
+            PaddleWidth,
+            PaddleHeight,
+            ScreenHeight,
+            Keys.W,
+            Keys.S
+        );
+
+        _player2 = new Paddle(
+            ScreenWidth - PaddleOffset - PaddleWidth,
+            (ScreenHeight - PaddleHeight) / 2,
+            PaddleWidth,
+            PaddleHeight,
+            ScreenHeight,
+            Keys.Up,
+            Keys.Down
+        );
+
+        _ball = new Ball(
+            (ScreenWidth - BallSize) / 2,
+            (ScreenHeight - BallSize) / 2,
+            BallSize,
+            ScreenWidth,
+            ScreenHeight
+        );
+
+        _scoreManager = new ScoreManager();
+        _gameState = GameState.Playing;
+        _previousKeyboardState = Keyboard.GetState();
 
         base.Initialize();
     }
@@ -27,25 +78,110 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        _pixelTexture = new Texture2D(GraphicsDevice, 1, 1);
+        _pixelTexture.SetData(new[] { Color.White });
+
+        _font = Content.Load<SpriteFont>("Font");
     }
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        var keyboardState = Keyboard.GetState();
+
+        if (keyboardState.IsKeyDown(Keys.Escape))
             Exit();
 
-        // TODO: Add your update logic here
+        if (_gameState == GameState.Playing)
+        {
+            _player1.Update(gameTime);
+            _player2.Update(gameTime);
+            _ball.Update(gameTime);
 
+            _ball.CheckPaddleCollision(_player1);
+            _ball.CheckPaddleCollision(_player2);
+
+            if (_ball.IsOutOfBounds())
+            {
+                if (_ball.ScoredOnLeft())
+                    _scoreManager.IncrementPlayer2Score();
+                else
+                    _scoreManager.IncrementPlayer1Score();
+
+                if (_scoreManager.HasWinner())
+                {
+                    _gameState = GameState.GameOver;
+                }
+                else
+                {
+                    _ball.Reset();
+                }
+            }
+        }
+        else if (_gameState == GameState.GameOver)
+        {
+            if (keyboardState.IsKeyDown(Keys.Space) && _previousKeyboardState.IsKeyUp(Keys.Space))
+            {
+                _scoreManager.Reset();
+                _ball.Reset();
+                _player1.Reset(PaddleOffset, (ScreenHeight - PaddleHeight) / 2);
+                _player2.Reset(ScreenWidth - PaddleOffset - PaddleWidth, (ScreenHeight - PaddleHeight) / 2);
+                _gameState = GameState.Playing;
+            }
+        }
+
+        _previousKeyboardState = keyboardState;
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
 
-        // TODO: Add your drawing code here
+        _spriteBatch.Begin();
+
+        DrawCenterLine();
+
+        _player1.Draw(_spriteBatch, _pixelTexture);
+        _player2.Draw(_spriteBatch, _pixelTexture);
+        _ball.Draw(_spriteBatch, _pixelTexture);
+
+        _scoreManager.Draw(_spriteBatch, _font, ScreenWidth, ScreenHeight);
+
+        if (_gameState == GameState.GameOver)
+        {
+            string winnerText = _scoreManager.GetWinnerText();
+            Vector2 winnerSize = _font.MeasureString(winnerText);
+            Vector2 winnerPos = new Vector2(
+                (ScreenWidth - winnerSize.X) / 2,
+                (ScreenHeight - winnerSize.Y) / 2
+            );
+            _spriteBatch.DrawString(_font, winnerText, winnerPos, Color.Yellow);
+
+            string restartText = "Press SPACE to restart";
+            Vector2 restartSize = _font.MeasureString(restartText);
+            Vector2 restartPos = new Vector2(
+                (ScreenWidth - restartSize.X) / 2,
+                (ScreenHeight - restartSize.Y) / 2 + 40
+            );
+            _spriteBatch.DrawString(_font, restartText, restartPos, Color.White);
+        }
+
+        _spriteBatch.End();
 
         base.Draw(gameTime);
+    }
+
+    private void DrawCenterLine()
+    {
+        int dashHeight = 10;
+        int dashGap = 10;
+        int lineWidth = 2;
+        int centerX = ScreenWidth / 2 - lineWidth / 2;
+
+        for (int y = 0; y < ScreenHeight; y += dashHeight + dashGap)
+        {
+            Rectangle dash = new Rectangle(centerX, y, lineWidth, dashHeight);
+            _spriteBatch.Draw(_pixelTexture, dash, Color.White);
+        }
     }
 }
