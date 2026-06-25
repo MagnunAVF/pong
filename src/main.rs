95f61b1a97a885518d79a53bb5db95cf4ba56789
MagnunAVF <magnunavf@gmail.com>
2026-06-25 15:19:15 -0300

    Initialize Bevy app with a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use bevy::{prelude::*, window::WindowResolution};
+
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Pong".into(),
+                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                resizable: false,
+                ..default()
+            }),
+            ..default()
+        }))
+        .run();
 }
